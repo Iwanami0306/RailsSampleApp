4afec8cbb554cd8e0b4f24893e8d5e561d297d1b
IwanamiHaruka <s1711432@s.tsukuba.ac.jp>
2022-04-23 17:01:12 +0900

    “基本的なツイート機能の実装”

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get '/:nickname', to: 'users#show'
+  get 'users/index'
   root 'static_pages#home'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  resources :tweets, only:[:create,:destroy]
 end
