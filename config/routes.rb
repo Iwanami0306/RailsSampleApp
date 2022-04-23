b7a30b621b290dd68bce6e5337aa0ac4139570c9
IwanamiHaruka <s1711432@s.tsukuba.ac.jp>
2022-04-23 17:36:32 +0900

    “フォロー機能の追加”

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get '/:nickname/followers', to:'users#show_followers'
+  get '/:nickname/followings', to:'users#show_followings'
   get '/:nickname', to: 'users#show'
   get 'users/index'
   root 'static_pages#home'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   resources :tweets, only:[:create,:destroy]
+  resources :follow_relationships, only:[:create,:destroy]
 end
