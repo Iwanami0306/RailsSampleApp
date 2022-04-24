cf404f450c6a424ceada61034451358a38dd3268
IwanamiHaruka <s1711432@s.tsukuba.ac.jp>
2022-04-24 15:57:51 +0900

    リプライ機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  post '/tweets/reply'
-  resources :tweets, only:[:create,:destroy,:show]
-  get '/:nickname/followers', to:'users#show_followers'
-  get '/:nickname/followings', to:'users#show_followings'
-  get '/:nickname', to: 'users#show'
   get 'users/index'
   root 'static_pages#home'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  resources :tweets, only:[:create,:destroy]
+  resources :tweets, only:[:create,:destroy,:show]
+  get '/:nickname/followers', to:'users#show_followers'
+  get '/:nickname/followings', to:'users#show_followings'
+  get '/:nickname', to: 'users#show'
+  post '/tweets/reply'
   resources :follow_relationships, only:[:create,:destroy]
 end
